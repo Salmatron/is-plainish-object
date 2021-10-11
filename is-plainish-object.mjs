72a2fa7f196a8ea5a3a5298e54b2a53603a4ba56
salmatron <salmanabbas007@gmail.com>
2021-10-11 21:26:01 +0000

    Published to npm as v0.1.0

---
+++ is-plainish-object.mjs
@@ -0,0 +1,64 @@
+
+const ObjectPrototype = Object.prototype;
+
+const getPrototypeOf = Object.getPrototypeOf;
+const getOwnPropertyNames = Object.getOwnPropertyNames;
+
+const toStringCat = Function.prototype.call.bind(ObjectPrototype.toString);
+
+const cache = new WeakMap();
+
+export function isPlainishObject(obj) {
+    if (!obj || (typeof obj !== 'object')) {
+        return false;
+    }
+
+    const cached = cache.get(obj);
+
+    if (typeof cached === 'boolean') {
+        return cached;
+    }
+
+    let result = false;
+
+    try {
+        if (toStringCat(obj) !== '[object Object]') {
+            return result;
+        }
+
+        const directProto = getPrototypeOf(obj);
+
+        if (typeof directProto !== 'object') {
+            return result;
+        }
+
+        if ((directProto === null) || (directProto === ObjectPrototype)) {
+            result = true;
+
+            return result;
+        }
+
+        const Ctor = obj.constructor;
+
+        if ((typeof Ctor !== 'function') || (Ctor !== directProto.constructor)) {
+            return result;
+        }
+
+        const topProto = getPrototypeOf(directProto);
+
+        if ((topProto !== null) && (topProto !== ObjectPrototype)) {
+            return result;
+        }
+
+        const protoProps = getOwnPropertyNames(directProto);
+
+        result = (protoProps.length === 1) && (protoProps[0] === 'constructor');
+
+        return result;
+    } finally {
+        cache.set(obj, result);
+    }
+
+    // eslint-disable-next-line no-unreachable
+    return false;
+}
